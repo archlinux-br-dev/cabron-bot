a33e1e36f1f5a6dabe5507d4b39c6b43fcc000a7
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-09 17:12:54 -0300

    Autocommit por Git-Cron: alterações: add 'cotacao3.php'

--- cotacao3.php
+++ cotacao3.php
@@ -6,15 +6,15 @@ error_reporting(E_ALL);
 //$url = file_get_contents('https://www.google.com.br/search?q=USD+to+BRL');
 $url = file_get_contents('https://duckduckgo.com/?q=USD+in+BRL&t=h_&ia=currency');
 
-$dom = new DomDocument;
+//$dom = new DomDocument;
 
 // We need to validate our document before refering to the id
-$dom->validateOnParse = true;
+//$dom->validateOnParse = true;
 
 
 //$dom->Load($url);
 
-$dom->loadHTML($url);
+//$dom->loadHTML($url);
 
 //$doc->getElementById('php-basics')->tagName
 
@@ -24,8 +24,15 @@ $dom->loadHTML($url);
 
 //$elem = $dom->getElementById('knowledge-currency__tgt-amount');
 //
-$elem = $dom->getElementById('zci--currency-amount-right');
-echo $elem[0]->nodeValue;
+function getElemById($id,$url)
+{
+    //$xpath = new DOMXPath($this->domDocument);
+    $xpath = new DOMXPath($url);
+    return $xpath->query("//*[@id='$id']")->item(0);
+}
+//
+$elem = getElemById('zci--currency-amount-right');
+echo $elem->nodeValue;
 
 //$result = $dom->getElementsByTagName('span')->item(0)->nodeValue;
 
