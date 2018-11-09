747c2032ccd6ca3520eab19801a59d59ea8d8e9f
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-09 17:05:50 -0300

    Autocommit por Git-Cron: alterações: add 'cotacao3.php'

--- cotacao3.php
+++ cotacao3.php
@@ -1,6 +1,10 @@
 <?php
 
-$url = file_get_contents('https://www.google.com.br/search?q=USD+to+BRL');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//$url = file_get_contents('https://www.google.com.br/search?q=USD+to+BRL');
+$url = file_get_contents('https://duckduckgo.com/?q=USD+in+BRL&t=h_&ia=currency');
 
 $dom = new DomDocument;
 
@@ -18,7 +22,9 @@ $dom->loadHTML($url);
 //
 //echo $dom->getElementById('knowledge-currency__tgt-amount')->tagName;
 
-$elem = $dom->getElementById('knowledge-currency__tgt-amount');
+//$elem = $dom->getElementById('knowledge-currency__tgt-amount');
+//
+$elem = $dom->getElementById('zci--currency-amount-right');
 echo $elem->nodeValue;
 
 //$result = $dom->getElementsByTagName('span')->item(0)->nodeValue;
