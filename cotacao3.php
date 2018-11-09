3701d38aa5ab87ca0687780fdd1f09d4017a5572
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-09 17:06:46 -0300

    Autocommit por Git-Cron: alterações: add 'cotacao3.php'

--- cotacao3.php
+++ cotacao3.php
@@ -24,7 +24,7 @@ $dom->loadHTML($url);
 
 //$elem = $dom->getElementById('knowledge-currency__tgt-amount');
 //
-$elem = $dom->getElementById('zci--currency-amount-right');
+$elem = $dom->getElementById('zci--currency-amount-right')->item(0);
 echo $elem->nodeValue;
 
 //$result = $dom->getElementsByTagName('span')->item(0)->nodeValue;
