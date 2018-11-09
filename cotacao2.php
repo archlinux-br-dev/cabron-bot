d454beeae5f108a8a1b0bba4d9d797de41b9e3d5
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-09 16:31:38 -0300

    Autocommit por Git-Cron: alterações: add 'cotacao2.php'

--- cotacao2.php
+++ cotacao2.php
@@ -1,73 +1,26 @@
 <?php
 
-// Mostra erros
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('default_socket_timeout', 300);
-
-$debug = true;
-$result = null;
-$error = null;
-$exception = null;
-$xml = null;
-
-
-// Definição da localização do arquivo WSDL
-$wsdl = 'https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl';
-
-// Soapclient PHP 5.0 ou Superior ( SOAP 1.1 e SOAP 1.2 )
-$client = new Soapclient($wsdl, array('trace' => 1, 'exceptions' => true));
-
-
-
-$xml =' <?xml version="1.0" encoding="UTF-8"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://publico.ws.casosdeuso.sgs.pec.bcb.gov.br" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-<SOAP-ENV:Body>
-<ns1:getUltimoValorVO>
-<in xsi:type="xsd:long">'. '206'.'</in>
-</ns1:getUltimoValorVO>
-</SOAP-ENV:Body>
-</SOAP-ENV:Envelope>';
-// Chamada do método SOAP
-try {
-    //$serie = 206;
-    $result = $client->getUltimoValorVO($xml, $serie);
-
-    if (is_object($result)) {
-        if (!$result->getUltimoValorVOResult) {
-            $error = $result->getUltimoValorVOError;
-        }
-    }
-
-    if (is_soap_fault($result)) {
-        trigger_error("SOAP Fault: (faultcode: {$result->faultcode}, faultstring: {$result->faultstring})", E_USER_ERROR);
-    }
-
-} catch (Exception $e) {
-    $exception = $e->getMessage();
+header('Content-type: text/html; charset=UTF-8');
+$ParametroPesquisa = "getUltimoValorXML";
+$Moeda = "1";
+// vamos evitar que o arquivo WSDL seja colocado no cache
+ini_set("soap.wsdl_cache_enabled", "0");
+$WsSOAP = new SoapClient("https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl");
+$DadosParaPesquisa = array("in0" => "$Moeda"); try {
+$ResultadoPesquisaWS = $WsSOAP -> $ParametroPesquisa($DadosParaPesquisa); 
+ 
+if (<a href="http://charlescorrea.com.br/blog/tag/isset">isset</a>($ResultadoPesquisaWS)) { $CotacaoMoedaWS = simplexml_load_string($ResultadoPesquisaWS);
+//print_r($CotacaoMoedaWS); $NomeMoeda = $CotacaoMoedaWS->SERIE->NOME;
+$ValorMoeda = $CotacaoMoedaWS->SERIE->VALOR;
+$DiaCotacaoMoeda = $CotacaoMoedaWS->SERIE->DATA->DIA;
+$MesCotacaoMoeda = $CotacaoMoedaWS->SERIE->DATA->MES;
+$AnoCotacaoMoeda = $CotacaoMoedaWS->SERIE->DATA->ANO;
+echo " $NomeMoeda
+<hr />
+Valor: $1 equivale à R$ $ValorMoeda <br /> Dia da Cotação: $DiaCotacaoMoeda/$MesCotacaoMoeda/$AnoCotacaoMoeda ";
+} else {
+exit('Falha ao abrir XML do BCB.');
 }
-
-if ($debug) {
-    echo '<h2>Request Headers</h2>';
-    echo '<pre>' . htmlspecialchars($client->__getLastRequestHeaders()) . '</pre>';
-
-    echo '<h2>Response Headers</h2>';
-    echo '<pre>' . htmlspecialchars($client->__getLastResponseHeaders()) . '</pre>';
-
-    echo '<h2>Request</h2>';
-    echo '<pre>' . htmlspecialchars($client->__getLastRequest()) . '</pre>';
-
-    echo '<h2>Response</h2>';
-    echo '<pre>' . htmlspecialchars($client->__getLastResponse()) . '</pre>';
-
-    echo '<h2>Debug</h2>';
-    echo '<pre>' . var_dump($result) . '</pre>';
-
-    echo '<h2>Exception</h2>';
-    echo '<pre>' . var_dump($exception) . '</pre>';
-
-    echo '<h2>Error</h2>';
-    echo '<pre>' . $error . '</pre>';
+} catch (Exception $Exception) {
+echo "ERRO AO REALIZAR A CAPTURA DE DADOS DO WEBSERVICE: " . $Exception -> getMessage();
 }
-
-?>
