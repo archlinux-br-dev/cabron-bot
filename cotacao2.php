dd6091c615ae31a61fa6f81fb3ab024b6f118e3a
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-09 16:39:01 -0300

    Autocommit por Git-Cron: alterações: add 'cotacao2.php'

--- cotacao2.php
+++ cotacao2.php
@@ -1,26 +1,15 @@
 <?php
-
-header('Content-type: text/html; charset=UTF-8');
-$ParametroPesquisa = "getUltimoValorXML";
-$Moeda = "1";
-// vamos evitar que o arquivo WSDL seja colocado no cache
-ini_set("soap.wsdl_cache_enabled", "0");
-$WsSOAP = new SoapClient("https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl");
-$DadosParaPesquisa = array("in0" => "$Moeda"); try {
-$ResultadoPesquisaWS = $WsSOAP -> $ParametroPesquisa($DadosParaPesquisa); 
- 
-if (<a href="http://charlescorrea.com.br/blog/tag/isset">isset</a>($ResultadoPesquisaWS)) { $CotacaoMoedaWS = simplexml_load_string($ResultadoPesquisaWS);
-//print_r($CotacaoMoedaWS); $NomeMoeda = $CotacaoMoedaWS->SERIE->NOME;
-$ValorMoeda = $CotacaoMoedaWS->SERIE->VALOR;
-$DiaCotacaoMoeda = $CotacaoMoedaWS->SERIE->DATA->DIA;
-$MesCotacaoMoeda = $CotacaoMoedaWS->SERIE->DATA->MES;
-$AnoCotacaoMoeda = $CotacaoMoedaWS->SERIE->DATA->ANO;
-echo " $NomeMoeda
-<hr />
-Valor: $1 equivale à R$ $ValorMoeda <br /> Dia da Cotação: $DiaCotacaoMoeda/$MesCotacaoMoeda/$AnoCotacaoMoeda ";
-} else {
-exit('Falha ao abrir XML do BCB.');
-}
-} catch (Exception $Exception) {
-echo "ERRO AO REALIZAR A CAPTURA DE DADOS DO WEBSERVICE: " . $Exception -> getMessage();
-}
+  //Read eurofxref-daily.xml file in memory
+  //For this command you will need the config
+  //option allow_url_fopen=On (default)
+  $XMLContent=file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+  foreach($XMLContent as $line){
+    if(preg_match("/currency='([[:alpha:]]+)'/",$line,$currencyCode)){
+      if(preg_match("/rate='([[:graph:]]+)'/",$line,$rate)){
+        if ($currencyCode[1] == "BRL") {
+          echo $rate[1];
+        }
+      }
+    }
+  }
+?>
