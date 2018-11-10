ef256a53e3d59d90489065ba5ac5ca2c41669678
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-11-10 10:00:02 -0300

    Autocommit por Git-Cron: alterações: add 'cotacao3.php'

--- cotacao3.php
+++ cotacao3.php
@@ -6,15 +6,15 @@ error_reporting(E_ALL);
 //$url = file_get_contents('https://www.google.com.br/search?q=USD+to+BRL');
 $url = file_get_contents('https://duckduckgo.com/?q=USD+in+BRL&t=h_&ia=currency');
 
-//$dom = new DomDocument;
+$dom = new DomDocument;
 
 // We need to validate our document before refering to the id
-//$dom->validateOnParse = true;
+$dom->validateOnParse = true;
 
 
 //$dom->Load($url);
 
-//$dom->loadHTML($url);
+$dom->loadHTML($url);
 
 //$doc->getElementById('php-basics')->tagName
 
